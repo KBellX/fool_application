6adf23d00fd5d7a219850335b429262f36a3a8e3
KBellX <kingbellx@163.com>
2019-01-19 01:01:56 +0800

    db

---
+++ config.php
@@ -0,0 +1,22 @@
+<?php
+
+$CONFIG = [
+    'database' => [
+        'read' => [
+            'host' => '192.168.211.130',
+            'dbname' => 'group_buy_shop',
+            'username' => 'root',
+            'password' => '123456',
+            'port' => 3306,
+            'charset' => 'utf8'
+        ],
+        'write' => [
+            'host' => '192.168.211.130',
+            'dbname' => 'study',
+            'username' => 'root',
+            'password' => '123456',
+            'port' => 3306,
+            'charset' => 'utf8'
+        ]
+    ],
+];
