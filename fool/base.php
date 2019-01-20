a3c62afbbfb6610123b2abe42acd4524b6ef03ff
KBellX <kingbellx@163.com>
2019-01-21 01:02:08 +0800

    improve autoload

--- fool/base.php
+++ fool/base.php
@@ -1,7 +1,18 @@
 <?php
 
-namespace fool;
+defined("FOOL_PATH") || define("FOOL_PATH", ROOT_PATH . '/fool');
+defined("APP_PATH") || define("APP_PATH", ROOT_PATH . '/app');
+define('DS', DIRECTORY_SEPARATOR);
+define('EXT', '.php');
+defined("EXTEND_PATH") || define("EXTEND_PATH", ROOT_PATH . '/extend');
 
-require __DIR__ . '/Loader.php';
+require FOOL_PATH . '/Loader.php';
 
-Loader::register();
+
+// 注册自动加载类
+\fool\Loader::register();
+
+// 注册异常处理
+
+// 加载配置文件
+require_once ROOT_PATH . '/config.php';
--- index.php
+++ index.php
@@ -1,20 +1,5 @@
 <?php
-// namespace fool;
 
 defined("ROOT_PATH") || define("ROOT_PATH", __DIR__);
-defined("FOOL_PATH") || define("FOOL_PATH", ROOT_PATH . '/fool');
-
-require_once ROOT_PATH . '/config.php';
-
-// 自动加载
 
 require __DIR__ . '/fool/base.php';
-
-use app\controller\User;
-
-$user = new User();
-$user->index();
-
-use fool\Db;
-
-$db = Db::instance();
